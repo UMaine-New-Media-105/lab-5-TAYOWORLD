3877a0b640a29d5f5af35fb0a7df70175f935457
TAYOWORLD <124199388+TAYOWORLD@users.noreply.github.com>
2023-03-01 14:00:49 -0500

    T bouncing around

---
+++ BouncingT.js
@@ -0,0 +1,54 @@
+let x;
+let y;
+let speedX;
+let speedY;
+
+function setup() {
+  createCanvas(400, 400);
+  
+  x = 5;
+  y = 8;
+  speedX = 3;
+  speedY = 9;
+}
+
+function draw() {
+  background(44,130,87);
+  
+  x = x + speedX;
+  y = y + speedY;
+  tFace(x,y,.3);
+  if (x >= 300 || x <= 0){
+    speedX = speedX * -1
+      
+ } 
+
+  if(y >= 300 || y <= 0){
+    speedY = speedY * -1
+  } 
+
+}
+
+function tFace(x,y,s){
+  push();
+  translate(x,y)
+  scale(s)
+  beginShape();
+  fill(44, 68,117)
+vertex(30,15);
+vertex(200,15);
+vertex(200, 75);
+vertex(30, 75);
+endShape(CLOSE);
+  
+  
+   beginShape();
+  fill(44, 68,117)
+  noStroke();
+vertex(80, 75);
+vertex(150,75);
+vertex(150,200);
+vertex(80,200);
+endShape(CLOSE);
+pop();
+}
